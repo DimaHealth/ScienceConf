85447c432f410d9b650937f6d9d4aaa38293c3fe
Sergey Medgaus <medgaus-sergey@yandex.ru>
2016-04-21 11:54:43 +0300

    Добавление таблицы Страны

---
+++ _www/templates/addCountry.php
@@ -0,0 +1,28 @@
+<html>
+   <head>
+  
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link href="favicon.ico" rel="shortcut icon">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Добавление страны</title>  
+  
+</head>
+<body>
+ 
+  <form name="addform" action="?action=add&IDCountry=<?= htmlspecialchars($id) ?>" method="POST"> 
+
+  <table class="table" > 
+<tr> 
+  <td>Страна</td>
+  <td><input type="text" name="Country" value="" required=" " /></td>
+  </tr>
+  
+  <tr>
+  <td><input type="submit" value="Добавить"></td>
+  <td><button type="button" onClick="history.back();">Отменить</button></td>
+   </tr>
+  </table>
+  </form>
+  </body>
+  </html>
