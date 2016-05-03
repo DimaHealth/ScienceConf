61454f081ff3ec0fa683be92188ad171d5f9122a
Sergey Medgaus <medgaus-sergey@yandex.ru>
2016-05-03 09:00:18 +0300

    Добавлена таблица Секции с возможностью добавления, редактирования и удаления записей.

---
+++ _www/templates/addSection.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <link href="favicon.ico" rel="shortcut icon">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Добавление секции</title>
+    </head>
+
+    <body>
+        <form name="addform" action="?action=add" method="POST"> 
+            <table class="table" > 
+                <tr> 
+                    <td>Секция</td>
+                    <td><input type="text" name="Section" value="" required="" /></td>
+                </tr>
+
+                <tr> 
+                    <td>Мероприятие</td>
+                    <td>
+                        <?php
+                            require_once("dbconnect.php");
+                            $sql = "SELECT * FROM events";
+                            $result_select = mysqli_query($connect, $sql);
+
+                            /*Выпадающий список*/
+                            echo "<select  name = CodeEvent>";
+                            while($arr = mysqli_fetch_array($result_select)) {
+                                echo "<option value = '$arr[0]' > $arr[1]</option>";
+                            }
+                            echo "</select>";
+                        ?>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td><input type="submit" value="Добавить"></td>
+                    <td><button type="button" onClick="history.back();">Отменить</button></td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
